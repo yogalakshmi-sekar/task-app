687f8b54b1e88a9da4be860b038d25f24d117dc6
yogalakshmi-sekar <yogacs232@rmkcet.ac.in>
2026-07-09 01:17:54 +0530

    fix port for railway

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const DB = path.join(__dirname, 'tasks.json');
 
 app.use(express.json());
